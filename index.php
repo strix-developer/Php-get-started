88f46531d84516cfc28afb8c8c7ea24e8a807dd9
Himanshu Thakur <110151861+zestergaming@users.noreply.github.com>
2022-08-01 14:58:21 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -3,36 +3,41 @@
 
 
 $school = array(
-    "Students" => array("Total  students" => '200'),
-    "Class" => array("section" => 'A,B,C'),
-    "Section-A" => array("name" => 'Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika', 'Roll.No' => ['Himanshu' => '1', 'Priya' => '2', 'Lakshay' => '3', 'Rahul' => '4', 'Hina' => '5', 'Arti' => '6', 'Miku' => '7', 'Lalit' => '8', 'Kanika' => '9'], 'Subject' => 'Chemistry,Maths,Computer,Physics,English', 'Totalstudents' => '9', 'Subject Timing' => ['Chemistry' => '9:00Am', 'Maths' => '10:00Am', 'Computer' => '11:00Am', 'Physics' => '1:00Pm', 'English' => '2:00Pm'], 'Teacher Name' => ['Chemistry' => 'Sunita Singh', 'Maths' => 'Varun Thakur', 'Computer' => 'Joyti Singh', 'Physics' => 'Sameer Thakur', 'English' => 'Priya sharma'], 'Building' => ['chemistry' => 'Block-A , Room.No-1 , Ac-Room', 'Maths' => 'Block-A , Room.No-2 , Ac-Room', 'Computer' => 'Block-B , Room.No-35  , Ac-Room', 'Physics' => 'Block-B , Room.No-30 , Ac-Room', 'English' => 'Block-C , Room.No-50 , Ac-Room']),
-    "Section-B" => array("name" => 'Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika,Dimpu', 'Roll.No' => ['Himanshu' => '10', 'Priya' => '11', 'Lakshay' => '12', 'Rahul' => '13', 'Hina' => '14', 'Arti' => '15', 'Miku' => '16', 'Lalit' => '17', 'Kanika' => '18', 'Dimpu' => '19'], 'Subject' => 'Computer,Physics,Chemistry,Maths,English', 'Totalstudents' => '10', 'Subject Timing' => ['Computer' => '9:00Am', 'Physics' => '10:00Am', 'Chemistry' => '11:00Am', 'Maths' => '1:00Pm', 'English' => '2:00Pm'], 'Teacher Name' => ['Chemistry' => 'Sunita Singh', 'Maths' => 'Varun Thakur', 'Computer' => 'Joyti Singh', 'Physics' => 'Sameer Thakur', 'English' => 'Priya sharma'],  'Building' => ['chemistry' => 'Block-A , Room.No-1 , Ac-Room', 'Maths' => 'Block-A , Room.No-2 , Ac-Room', 'Computer' => 'Block-B , Room.No-35  , Ac-Room', 'Physics' => 'Block-B , Room.No-30 , Ac-Room', 'English' => 'Block-C , Room.No-50 , Ac-Room']),
-    "Section-C" => array("name" => 'Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika,Dimpu', 'Roll.No' => ['Himanshu' => '20', 'Priya' => '21', 'Lakshay' => '22', 'Rahul' => '23', 'Hina' => '24', 'Arti' => '25', 'Miku' => '26', 'Lalit' => '27', 'Kanika' => '28', 'Dimpu' => '29'], 'Subject' => 'Physics,English,Maths,Chemistry,Computer', 'Totalstudents' => '10', 'Subject' => 'Physics,Computer,Maths,Chemistry,English', 'Totalstudents' => '10', 'Subject Timing' => ['Physics' => '9:00Am', 'Computer' => '10:00Am', 'Maths' => '11:00Am', 'Chemistry' => '1:00Pm', 'English' => '2:00Pm'], 'Teacher Name' => ['Chemistry' => 'Sunita Singh', 'Maths' => 'Varun Thakur', 'Computer' => 'Joyti Singh', 'Physics' => 'Sameer Thakur', 'English' => 'Priya sharma'],  'Building' => ['chemistry' => 'Block-A , Room.No-1 , Ac-Room', 'Maths' => 'Block-A , Room.No-2 , Ac-Room', 'Computer' => 'Block-B , Room.No-35  , Ac-Room', 'Physics' => 'Block-B , Room.No-30 , Ac-Room', 'English' => 'Block-C , Room.No-50 , Ac-Room']),
-    "Teacher" => array("Total teacher" => '10'),
-    "Teacher Name" => ['Sunita Singh', 'Varun Thakur', 'Jyoti Singh', 'Sameer Thakur', 'Priya Sharma', 'Manjeet Thakur', 'Ketan Thakur', 'Ghanshyam Singh', 'Twinkle Thakur', 'Ashu Rana'],
-    "Teacher Qualification" => [
-        'Sunita Singh' => 'Teaching Certificate(English):
+    "Students" => array(
+        "Total  students" => '200',
+        "Class" => array(
+            "Section-A" => array("name" => 'Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika', 'Roll.No' => ['Himanshu' => '1', 'Priya' => '2', 'Lakshay' => '3', 'Rahul' => '4', 'Hina' => '5', 'Arti' => '6', 'Miku' => '7', 'Lalit' => '8', 'Kanika' => '9'], 'Subject' => 'Chemistry,Maths,Computer,Physics,English', 'Totalstudents' => '9', 'Subject Timing' => ['Chemistry' => '9:00Am', 'Maths' => '10:00Am', 'Computer' => '11:00Am', 'Physics' => '1:00Pm', 'English' => '2:00Pm'], 'Teacher Name' => ['Chemistry' => 'Sunita Singh', 'Maths' => 'Varun Thakur', 'Computer' => 'Joyti Singh', 'Physics' => 'Sameer Thakur', 'English' => 'Priya sharma'], 'Building' => ['chemistry' => 'Block-A , Room.No-1 , Ac-Room', 'Maths' => 'Block-A , Room.No-2 , Ac-Room', 'Computer' => 'Block-B , Room.No-35  , Ac-Room', 'Physics' => 'Block-B , Room.No-30 , Ac-Room', 'English' => 'Block-C , Room.No-50 , Ac-Room']),
+            "Section-B" => array("name" => 'Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika,Dimpu', 'Roll.No' => ['Himanshu' => '10', 'Priya' => '11', 'Lakshay' => '12', 'Rahul' => '13', 'Hina' => '14', 'Arti' => '15', 'Miku' => '16', 'Lalit' => '17', 'Kanika' => '18', 'Dimpu' => '19'], 'Subject' => 'Computer,Physics,Chemistry,Maths,English', 'Totalstudents' => '10', 'Subject Timing' => ['Computer' => '9:00Am', 'Physics' => '10:00Am', 'Chemistry' => '11:00Am', 'Maths' => '1:00Pm', 'English' => '2:00Pm'], 'Teacher Name' => ['Chemistry' => 'Sunita Singh', 'Maths' => 'Varun Thakur', 'Computer' => 'Joyti Singh', 'Physics' => 'Sameer Thakur', 'English' => 'Priya sharma'],  'Building' => ['chemistry' => 'Block-A , Room.No-1 , Ac-Room', 'Maths' => 'Block-A , Room.No-2 , Ac-Room', 'Computer' => 'Block-B , Room.No-35  , Ac-Room', 'Physics' => 'Block-B , Room.No-30 , Ac-Room', 'English' => 'Block-C , Room.No-50 , Ac-Room']),
+            "Section-C" => array("name" => 'Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika,Dimpu', 'Roll.No' => ['Himanshu' => '20', 'Priya' => '21', 'Lakshay' => '22', 'Rahul' => '23', 'Hina' => '24', 'Arti' => '25', 'Miku' => '26', 'Lalit' => '27', 'Kanika' => '28', 'Dimpu' => '29'], 'Subject' => 'Physics,English,Maths,Chemistry,Computer', 'Totalstudents' => '10', 'Subject' => 'Physics,Computer,Maths,Chemistry,English', 'Totalstudents' => '10', 'Subject Timing' => ['Physics' => '9:00Am', 'Computer' => '10:00Am', 'Maths' => '11:00Am', 'Chemistry' => '1:00Pm', 'English' => '2:00Pm'], 'Teacher Name' => ['Chemistry' => 'Sunita Singh', 'Maths' => 'Varun Thakur', 'Computer' => 'Joyti Singh', 'Physics' => 'Sameer Thakur', 'English' => 'Priya sharma'],  'Building' => ['chemistry' => 'Block-A , Room.No-1 , Ac-Room', 'Maths' => 'Block-A , Room.No-2 , Ac-Room', 'Computer' => 'Block-B , Room.No-35  , Ac-Room', 'Physics' => 'Block-B , Room.No-30 , Ac-Room', 'English' => 'Block-C , Room.No-50 , Ac-Room']),
+        ),
+    ),
+    "Teacher" => array(
+        "Total teacher" => '10',
+        "Teacher Name" => ['Sunita Singh', 'Varun Thakur', 'Jyoti Singh', 'Sameer Thakur', 'Priya Sharma', 'Manjeet Thakur', 'Ketan Thakur', 'Ghanshyam Singh', 'Twinkle Thakur', 'Ashu Rana'],
+        "Teacher Qualification" => [
+            'Sunita Singh' => 'Teaching Certificate(English):
                                B.Ed(UK); Started M.Ed',
-        'Varun Thakur' => 'Teaching Certificate(English):
+            'Varun Thakur' => 'Teaching Certificate(English):
                                 B.Ed(UK); Started M.Ed',
-        'Jyoti Singh' => 'Teaching Certificate(English):
+            'Jyoti Singh' => 'Teaching Certificate(English):
                                 B.Ed(UK); Started M.Ed',
-        'Sameer Thakur' => 'Teaching Certificate(English):
+            'Sameer Thakur' => 'Teaching Certificate(English):
                                 B.Ed(UK); Started M.Ed',
-        'Priya Sharma' => 'Teaching Certificate(English):
+            'Priya Sharma' => 'Teaching Certificate(English):
                                 B.Ed(UK); Started M.Ed',
-        'Manjeet Thakur' => 'Teaching Certificate(English):
+            'Manjeet Thakur' => 'Teaching Certificate(English):
                                 B.Ed(UK); Started M.Ed',
-        'Ketan Thakur' => 'Teaching Certificate(English):
+            'Ketan Thakur' => 'Teaching Certificate(English):
                                 B.Ed(UK); Started M.Ed',
-        'Ghanshyam Singh' => 'Teaching Certificate(English):
+            'Ghanshyam Singh' => 'Teaching Certificate(English):
                                 B.Ed(UK); Started M.Ed',
-        'Twinkle Thakur' => 'Teaching Certificate(English):
+            'Twinkle Thakur' => 'Teaching Certificate(English):
                                 B.Ed(UK); Started M.Ed',
-        'Ashu Rana' => 'Teaching Certificate(English):
+            'Ashu Rana' => 'Teaching Certificate(English):
                                 B.Ed(UK); Started M.Ed',
-    ],
-    "Teacher Subject" => ['Sunita Singh' => 'Chemistry', 'Varun Thakur' => 'Maths', 'Jyoti Singh' => 'Computer', 'Sameer Thakur' => 'Physics', 'Priya Sharma' => 'English', 'Manjeet Thakur' => 'Sports', 'Ketan Thakur' => 'Music', 'Ghanshyam Singh' => 'Principle', 'Twinkle Thakur' => 'Dance', 'Ashu Rana' => 'Vice Principle']
+        ],
+        "Teacher Subject" => ['Sunita Singh' => 'Chemistry', 'Varun Thakur' => 'Maths', 'Jyoti Singh' => 'Computer', 'Sameer Thakur' => 'Physics', 'Priya Sharma' => 'English', 'Manjeet Thakur' => 'Sports', 'Ketan Thakur' => 'Music', 'Ghanshyam Singh' => 'Principle', 'Twinkle Thakur' => 'Dance', 'Ashu Rana' => 'Vice Principle']
+    ),
 
 
 );
