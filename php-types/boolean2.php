aa7a367d322444e24b544e94578542c8e5a304bd
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-08-19 14:16:44 +0530

    commit changes

---
+++ php-types/boolean2.php
@@ -0,0 +1,53 @@
+<?php
+ $array=array(10,20,30,40,60.0);   //index array
+ $myarray=[10,20,30];
+  var_dump($array);
+
+  
+//associative array
+$name=array('name'=>"abc",'lastname'=>'xyz','age'=>25, array(23,
+  array('myname'=>'Tarun')
+
+
+)
+
+
+
+
+);
+
+print_r($name);
+ 
+
+$SCH=array(
+    [
+        'school'=>['student'=>[
+
+
+
+
+
+        ],'teachesrs']
+    ]
+
+
+    
+    );
+print_r($SCH);
+
+
+
+$scho=array('school'=>['students'=>[
+    'name'=>'jon','class'=>"10",'section'=>'a'],
+
+
+'teachers'=>['name'=>['guru','guru2','guru3'],'subject'=>['hindi','english','math']]]
+
+
+
+);
+print_r($scho);
+?>
+
+
+ 
