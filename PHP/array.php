626e89b4a91b29b9e20b6f52a8437ce40c636a3c
Himanshu Thakur <110151861+zestergaming@users.noreply.github.com>
2022-08-26 12:09:45 +0530

    commit

--- PHP/array.php
+++ PHP/array.php
@@ -2,12 +2,20 @@
 $school = array(
     //students Details
     "Students" => [
+        //Class
         "class" => [
             //Secton-A
             [
                 "Section-A" => [
-                    "Name" => 'Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika',
-                    'Roll.No' => ['Himanshu' => '1', 'Priya' => '2', 'Lakshay' => '3', 'Rahul' => '4', 'Hina' => '5', 'Arti' => '6', 'Miku' => '7', 'Lalit' => '8', 'Kanika' => '9'],
+                    'Name & Roll.No' => ['Himanshu' => '1', 
+                    'Priya' => '2',
+                    'Lakshay' => '3',
+                    'Rahul' => '4',
+                    'Hina' => '5',
+                    'Arti' => '6',
+                    'Miku' => '7',
+                    'Lalit' => '8',
+                    'Kanika' => '9'],
                     'Subject' => 'Chemistry,Maths,Computer,Physics,English',
                     'Totalstudents' => '9',
                     'Subject Timing' => ['Chemistry' => '9:00Am', 'Maths' => '10:00Am', 'Computer' => '11:00Am', 'Physics' => '1:00Pm', 'English' => '2:00Pm'],
@@ -18,8 +26,15 @@ $school = array(
             //Section-B
             [
                 "Section-B" => [
-                    "Name" => 'Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika',
-                    'Roll.No' => ['Himanshu' => '10', 'Priya' => '11', 'Lakshay' => '12', 'Rahul' => '13', 'Hina' => '14', 'Arti' => '15', 'Miku' => '16', 'Lalit' => '17', 'Kanika' => '18'],
+                    'Name & Roll.No' => ['Himanshu' => '10',
+                    'Priya' => '11',
+                    'Lakshay' => '12',
+                    'Rahul' => '13',
+                    'Hina' => '14',
+                    'Arti' => '15',
+                    'Miku' => '16',
+                    'Lalit' => '17',
+                    'Kanika' => '18'],
                     'Subject' => 'Chemistry,Maths,Computer,Physics,English',
                     'Totalstudents' => '9',
                     'Subject Timing' => ['Chemistry' => '9:00Am', 'Maths' => '10:00Am', 'Computer' => '11:00Am', 'Physics' => '1:00Pm', 'English' => '2:00Pm'],
@@ -31,8 +46,15 @@ $school = array(
             //Section-C
             [
                 "Section-C" => [
-                    "Name" => 'Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika',
-                    'Roll.No' => ['Himanshu' => '19', 'Priya' => '20', 'Lakshay' => '21', 'Rahul' => '22', 'Hina' => '23', 'Arti' => '24', 'Miku' => '25', 'Lalit' => '26', 'Kanika' => '27'],
+                    'Name & Roll.No' => ['Himanshu' => '19',
+                    'Priya' => '20',
+                    'Lakshay' => '21',
+                    'Rahul' => '22',
+                    'Hina' => '23',
+                    'Arti' => '24',
+                    'Miku' => '25',
+                    'Lalit' => '26',
+                    'Kanika' => '27'],
                     'Subject' => 'Chemistry,Maths,Computer,Physics,English',
                     'Totalstudents' => '9',
                     'Subject Timing' => ['Chemistry' => '9:00Am', 'Maths' => '10:00Am', 'Computer' => '11:00Am', 'Physics' => '1:00Pm', 'English' => '2:00Pm'],
@@ -40,10 +62,10 @@ $school = array(
                     'Building' => ['chemistry' => 'Block-A , Room.No-1 , Ac-Room', 'Maths' => 'Block-A , Room.No-2 , Ac-Room', 'Computer' => 'Block-B , Room.No-35  , Ac-Room', 'Physics' => 'Block-B , Room.No-30 , Ac-Room', 'English' => 'Block-C , Room.No-50 , Ac-Room']
                 ],
             ],
-            //Total Students
-            [
-                "Total Students" => ['200']
-            ],
+        ],
+        //Total Students
+        [
+            "Total Students" => ['200', "Name" => ['Himanshu,Priya,Lakshay,Rahul,Hina,Arti,Miku,Lalit,Kanika'],]
         ],
     ],
     //Teachers Details
@@ -71,14 +93,15 @@ $school = array(
         [
             ['Subjects' => ['Sunita Singh' => 'Chemistry', 'Varun Thakur' => 'Maths', 'Jyoti Singh' => 'Computer', 'Sameer Thakur' => 'Physics', 'Priya Sharma' => 'English', 'Manjeet Thakur' => 'Sports', 'Ketan Thakur' => 'Music', 'Ghanshyam Singh' => 'Principle', 'Twinkle Thakur' => 'Dance', 'Ashu Rana' => 'Vice Principle'],],
         ],
-        //Total Teachers
-        [
-            'Total  Teachers' => ['10'],
-        ]
     ],
+    //Total Teachers
+    [
+        'Total  Teachers' => ['10'],
+    ]
+
 
 );
 
 
 
-echo $school['Students']['class']['0']['Section-A']['Teacher Name']['Computer'];
+print_r($school);
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+include __DIR__."\PHP\array.php";
+
+?>
