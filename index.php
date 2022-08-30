56a453ac47fbe7e2476d0b552659458c287c5b36
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-08-30 11:48:25 +0530

    commit changes

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-include __DIR__."/php/arrays/flowchart.php";
+//include __DIR__."/php/arrays/flowchart.php";
 
 // include __DIR__."/html/internal-CSS/table.html";
+
+include __DIR__."/php/variables";
 
 ?>
