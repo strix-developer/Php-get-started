4ebdb5bdd5a69ee8792981bc7df3c408a72cd53b
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-08-30 14:50:52 +0530

    commit changes

---
+++ php/form/get.php
@@ -0,0 +1,7 @@
+My name is : <?php echo $_GET['username']; ?> </br>
+My roll number is : <?php echo $_GET['rollnumber']; ?> </br>
+My class is : <?php echo $_GET['class']; ?> </br>
+My phone number is : <?php echo $_GET['phonenumber']; ?> </br>
+My email is : <?php echo $_GET['email']; ?> </br>
+My age is : <?php echo $_GET['age']; ?> </br>
+My section is :<?php echo $_GET['section']; ?> </br>
---
+++ php/form/post.php
@@ -0,0 +1,7 @@
+My name is : <?php echo $_POST['username']; ?> </br>
+My roll number is : <?php echo $_POST['rollnumber']; ?> </br>
+My class is : <?php echo $_POST['class']; ?> </br>
+My phone number is : <?php echo $_POST['phonenumber']; ?> </br>
+My email is : <?php echo $_POST['email']; ?> </br>
+My age is : <?php echo $_POST['age']; ?> </br>
+My section is :<?php echo $_POST['section']; ?> </br>
