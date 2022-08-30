f34c8502d13c38d671f9f189c2011c0e715e686e
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-08-30 11:44:50 +0530

    commit changes

---
+++ php/variables/localvariable.php
@@ -0,0 +1,17 @@
+<?php
+
+// function myTest()
+// {
+//     $x = 5;
+//     echo "Variable x inside function is: $x";
+// }
+// myTest();
+
+// echo "Variable x outside function is: $x";
+
+$strix="Strix is an organisation";
+$company='strix';
+ 
+echo $company;
+echo $$company;
+?>
