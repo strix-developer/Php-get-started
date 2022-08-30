9bf81f0f153a1d173395c9fa9f575aee8c9d332d
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-08-30 16:33:44 +0530

    commit changes

---
+++ form/action.php
@@ -0,0 +1,27 @@
+<html>
+
+<body>
+    <!--
+    My name is : <?php echo $_GET['username']; ?> </br>
+    My Father name is : <?php echo $_POST['fathername']; ?> </br>
+    My Mother name is : <?php echo $_POST['mothername']; ?> </br>
+    My roll number is : <?php echo $_GET['rollnumber']; ?> </br>
+    My class is : <?php echo $_GET['class']; ?> </br>
+    My phone number is : <?php echo $_GET['phonenumber']; ?> </br>
+    My email is : <?php echo $_GET['email']; ?> </br>
+    My age is : <?php echo $_GET['age']; ?> </br>
+    My section is :<?php echo $_GET['section']; ?> </br>
+-->
+
+    My name is : <?php echo $_POST['username']; ?> </br>
+    My Father name is : <?php echo $_POST['fathername']; ?> </br>
+    My Mother name is : <?php echo $_POST['mothername']; ?> </br>
+    My roll number is : <?php echo $_POST['rollnumber']; ?> </br>
+    My class is : <?php echo $_POST['class']; ?> </br>
+    My phone number is : <?php echo $_POST['phonenumber']; ?> </br>
+    My email is : <?php echo $_POST['email']; ?> </br>
+    My age is : <?php echo $_POST['age']; ?> </br>
+    My section is :<?php echo $_POST['section']; ?> </br>
+</body>
+
+</html>
