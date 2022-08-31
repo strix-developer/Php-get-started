0df1a7723e53db894aee261b87a242b86ea77f60
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-08-31 09:36:35 +0530

    commit

--- actionget.php
+++ actionget.php
@@ -2,15 +2,15 @@
 
 <body>
 
-    My name is : <?php echo $_GET['username']; ?> </br>
-    My Father name is : <?php echo $_GET['fathername']; ?> </br>
-    My Mother name is : <?php echo $_GET['mothername']; ?> </br>
-    My roll number is : <?php echo $_GET['rollnumber']; ?> </br>
-    My class is : <?php echo $_GET['class']; ?> </br>
-    My phone number is : <?php echo $_GET['phonenumber']; ?> </br>
-    My email is : <?php echo $_GET['email']; ?> </br>
-    My age is : <?php echo $_GET['age']; ?> </br>
-    My section is :<?php echo $_GET['section']; ?> </br>
+    My name is : <?php echo $_GET['username']; ?> <br>
+    My Father name is : <?php echo $_GET['fathername']; ?> <br>
+    My Mother name is : <?php echo $_GET['mothername']; ?> <br>
+    My roll number is : <?php echo $_GET['rollnumber']; ?> <br>
+    My class is : <?php echo $_GET['class']; ?> <br>
+    My phone number is : <?php echo $_GET['phonenumber']; ?> <br>
+    My email is : <?php echo $_GET['email']; ?> <br>
+    My age is : <?php echo $_GET['age']; ?> <br>
+    My section is :<?php echo $_GET['section']; ?> <br>
 
 </body>
 
