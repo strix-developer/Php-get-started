906f67775f94e8d78fc7167819017d70fad0e8a4
Himanshu Thakur <110151861+zestergaming@users.noreply.github.com>
2022-09-21 16:31:57 +0530

    commit change

--- PHP/operators.php
+++ PHP/operators.php
@@ -2,17 +2,43 @@
 
 $age=25;
 if($age>=18 or $age<=20){
-echo "This is my age";
+echo "This is my age  ";
 var_dump($age);
 }
 else{ echo "this is not my age";}
-/*$x = 100;  
+echo "<br>";
+$x = 100;  
 
 if (!($x <= 90)) {
     echo "ho gaya kya?";
-}*/
-
-
-
-
+}
+echo "<br>";
+$name = "Himanshu";
+$surname = " Thakur";
+echo $name . $surname;
+echo "<br>";
+$name1 = "Himanshu";
+$surname1 = " Thakur";
+$name1 .= $surname1;
+ echo $name1;
+ echo "<br>";
+ echo "<h3>Postincrement</h3>";
+ $a = 5;
+ echo "Should be 5: " . $a++ . "<br />\n";
+ echo "Should be 6: " . $a . "<br />\n";
+ 
+ echo "<h3>Preincrement</h3>";
+ $a = 5;
+ echo "Should be 6: " . ++$a . "<br />\n";
+ echo "Should be 6: " . $a . "<br />\n";
+ 
+ echo "<h3>Postdecrement</h3>";
+ $a = 5;
+ echo "Should be 5: " . $a-- . "<br />\n";
+ echo "Should be 4: " . $a . "<br />\n";
+ 
+ echo "<h3>Predecrement</h3>";
+ $a = 5;
+ echo "Should be 4: " . --$a . "<br />\n";
+ echo "Should be 4: " . $a . "<br />\n";
 ?>
