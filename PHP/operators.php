6fc12ab188ec2862d78ee1f7c6b1b676c69768e2
Himanshu Thakur <110151861+zestergaming@users.noreply.github.com>
2022-09-21 12:38:31 +0530

    commit chaange

--- PHP/operators.php
+++ PHP/operators.php
@@ -1,8 +1,18 @@
 <?php
-//Alphabets
-$s='A';
-for($n=0;$n<=26;$n++)
-{
-echo $s++.'-'.$n++;
-echo"<br>";
+
+$age=25;
+if($age>=18 or $age<=20){
+echo "This is my age";
+var_dump($age);
 }
+else{ echo "this is not my age";}
+/*$x = 100;  
+
+if (!($x <= 90)) {
+    echo "ho gaya kya?";
+}*/
+
+
+
+
+?>
