f617890ab23664c316aa49192df9dee901b53ce6
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-09-30 15:51:45 +0530

    commit changes

--- php/controlstructure/while.php
+++ php/controlstructure/while.php
@@ -4,7 +4,7 @@ $a = 0;
 while ($a <= 10) {
     $b = 0;
     while ($b <= $a) {
-        echo "&nbsp * &nbsp";
+        echo "&nbsp&#174&nbsp";
         $b++;
     }
     $a++;
@@ -16,7 +16,7 @@ $a = 10;
 while ($a >= 0) {
     $b = 0;
     while ($b <= $a) {
-        echo "&nbsp * &nbsp";
+        echo "&nbsp&#169&nbsp";
         $b++;
     }
     $a--;
@@ -26,36 +26,35 @@ echo "<br>";
 
 
 $a = 0;
-while ($a <= 10) {
+while ($a <= 15) {
     $b = 0;
     while ($b <= $a) {
-        echo "&nbsp ";
+        echo "&nbsp&nbsp";
         $b++;
     }
     $b = 0;
-    while ($b < 10 - $a) {
-        echo "&nbsp*";
+    while ($b < 15 - $a) {
+        echo "+";
         $b++;
     }
     $a++;
-    echo " <br> ";
+    echo "<br>";
 }
 echo "<br>";
-
 
 $a = 10;
 while ($a >= 0) {
     $b = 0;
     while ($b <= $a) {
-        echo "&nbsp ";
+        echo "&nbsp&nbsp";
         $b++;
     }
     $b = 0;
     while ($b < 10 - $a) {
-        echo "&nbsp +";
+        echo "&nbsp&nbsp&#162";
         $b++;
     }
     $a--;
-    echo " <br> ";
+    echo "<br>";
 }
 echo "<br>";
