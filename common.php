49986da89f9d53bad77ff58df39b268f9e926725
jiteshverma1 <jitesh.strix@gmail.com>
2022-10-01 11:27:40 +0530

    commit

---
+++ common.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "This is first paragraph";echo"<br>";
+// echo "not so useful";
+
+echo "This is second paragraph";echo"<br>";
+// echo "this must be useful";
+
+echo "you can check here more";echo"<br>";
+// echo "kuch nahi milega";
+
+?>
