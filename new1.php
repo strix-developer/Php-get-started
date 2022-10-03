86003069ef276764ad0aae163930f2eefdac7bb4
jiteshverma1 <jitesh.strix@gmail.com>
2022-10-03 10:29:35 +0530

    commit

---
+++ new1.php
@@ -0,0 +1,35 @@
+<?php
+
+echo "welcome";echo"<br>";
+
+   echo "This is a php page";echo"<br>";
+
+   echo $text="It is a long 
+established fact that a reader 
+will be distracted 
+by the readable content of a 
+page when looking at its layout. 
+The point of using Lorem Ipsum 
+is that it has a 
+more-or-less normal distribution of 
+letters, as opposed to using 'Content here, 
+content here', making it look like readable English. 
+Many desktop publishing packages and web 
+page editors now use Lorem Ipsum as their 
+default model text, and a search for 'lorem ipsum' 
+will uncover many web sites still in their infancy. 
+Various versions have evolved over the years, 
+sometimes by accident, sometimes on 
+purpose (injected humour and the like).
+";
+/*PHP started out as a small open source project that evolved as more and 
+more people found out how useful it was. Rasmus Lerdorf unleashed the first 
+version of PHP way back in 1994.
+
+PHP is a recursive acronym for "PHP: Hypertext Preprocessor".
+
+PHP is a server side scripting language that is embedded in HTML. It is used to
+ manage dynamic content, databases, session tracking, even build entire e-commerce sites.
+*/
+
+?>
