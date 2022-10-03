ac531923306a1c39a205ba07fb104d4a4fad0492
rishavdhiman1 <rishav.strix@gmail.com>
2022-10-03 13:58:38 +0530

    commit

---
+++ type.php
@@ -0,0 +1,21 @@
+<?php
+//boolean types
+$a = true;
+echo $a;
+echo "<br>";
+//integer 
+$a = 3;
+echo $a;
+echo "<br>";
+//float 
+$b = 1.1;
+echo "$b";
+echo "<br>";
+//string
+$a ='Arnold once said: "I\'ll be back"';
+echo $a;
+echo "<br>";
+//numeric string
+$a = "12345";
+echo "$a";
+?>
