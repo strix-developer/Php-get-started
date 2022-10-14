236d99d9c5f20a3da0c31ab5591e85a884d68bab
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-10-14 16:50:27 +0530

    commit

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 
 // include __DIR__."/php/variables/global.php";
 
-//include __DIR__ . "/form/studentpost.php";
+//include __DIR__."/form/studentpost.php";
 
-//include __DIR__ . "/php/ifelseif.php";
+//include __DIR__."/php/ifelseif.php";
 
-include __DIR__ . "/php/controlstructure/while.php";
+include __DIR__."/php/controlstructure/while.php";
