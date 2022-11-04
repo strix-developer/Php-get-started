fe16b070a2e95d1092ef6f50367353bdcfdd5870
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-11-04 16:45:24 +0530

    commit

--- register/core/support/helper.php
+++ register/core/support/helper.php
@@ -1,34 +1,31 @@
 <?php
 
-function action($register)
+function  action($register)
 {
-    $actions = __DIR__ . './action/' . $register . '.php';
-    if (has_action($actions)) {
+    $actions = __DIR__ . '/../support/action' . $register . '.php';
+    if (action($actions)) {
         return realpath($actions);
     } else {
         return __DIR__ . "/action/fallback.php";
-    }
-}
-function has_action($actions)
-{
-    return file_exists($actions);
-}
+    };
 
-
+    function has_action($actions)
+    {
+        return file_exists($actions);
+    };
+};
 
 function file_header()
 {
-    return  require_once __DIR__ . "/layout/header.php";
-}
-
+    return realpath(__DIR__ . "/layout/header.php");
+};
 
 function file_content()
 {
-    return require_once __DIR__ . "/layout/content.php";
-}
-
+    return realpath(__DIR__ . "/layout/content.php");
+};
 
 function file_footer()
 {
-    return  require_once __DIR__ . "/layout/footer.php";
-}
+  return realpath(__DIR__ . "/layout/footer.php");
+};
