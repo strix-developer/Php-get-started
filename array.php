6e968d18a54303a71fa53bf7917d1eb0d71029d6
rishavdhiman1 <114643314+rishavdhiman1@users.noreply.github.com>
2022-11-04 14:34:17 +0530

    Delete array.php

--- array.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cars = array("Volvo", "BMW", "Toyota");
-echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . "."; 
-echo"<br>";
-?>
-<?php
-$cars = array("Volvo", "BMW", "Toyota");
-echo count($cars);
-?> 
