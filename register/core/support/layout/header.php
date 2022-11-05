6c254af19d3b1c9234c28cc4ac87262c284aa2d1
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-11-05 12:24:53 +0530

    commit

--- register/core/support/layout/header.php
+++ register/core/support/layout/header.php
@@ -31,7 +31,7 @@
                         <div class="container-fluid">
                             <ul class="navbar-nav mx-auto">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="javascript:void(0)">Home</a>
+                                    <a class="nav-link" href="index.php">Home</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="javascript:void(0)">About</a>
