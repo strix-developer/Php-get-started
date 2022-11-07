67e907d76e58e990d7e35333bc586f93fa89c250
jiteshverna1 <jitesh.strix@gmail.com>
2022-11-07 14:14:38 +0530

    constant

--- constant/constant1.php
+++ constant/constant1.php
@@ -1,17 +1,17 @@
 <?php
 // Simple scalar value
-const CONSTANT = ' Hello World ';
+const CONSTANT = ' Hello World '."<br>";
 
 
 // Scalar expression
-const ANOTHER_CONST = CONSTANT.' Goodbye World ';
+const ANOTHER_CONST = CONSTANT.' Goodbye World '."<br>";
 echo ANOTHER_CONST;
 
 const ANIMALS = array(' dog ', ' cat ', ' bird ');
 echo ANIMALS[ 0 ]; 
 echo ANIMALS[ 1 ];
 echo ANIMALS[ 2 ];
-
+echo "<br>";
 
 ?>
 
@@ -28,7 +28,21 @@ const bd = ' bar ';
 
 echo foo;   // Prints out: Hello
 echo bar;
-echo bd;
+echo bd ."<br>";
+
+echo PHP_VERSION."<br>";
+echo PHP_MAJOR_VERSION."<br>";
+echo PHP_MINOR_VERSION. "<br>";
+echo PHP_RELEASE_VERSION."<br>";
+echo PHP_VERSION_ID."<br>";
+echo PHP_EXTRA_VERSION."<br>";
+echo PHP_DEBUG."<br>";
+
+//magic constants
+echo __LINE__."<br>";
+echo __FILE__."<br>";
+echo __DIR__."<br>";
+
 
 
 ?>
