d412becc30a148c1fd671d9cf002383501eab843
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-11-23 15:28:51 +0530

    commit

--- register/logout.php
+++ register/logout.php
@@ -1,13 +1,12 @@
  <?php
+  //Initialize session data
+  session_start();
 
-   //Initialize session data
-   session_start();
+  //Free all session variables
+  session_unset();
 
-   //Free all session variables
-   session_unset();
+  //Destroys all data registered to a session
+  session_destroy();
 
-   //Destroys all data registered to a session
-   session_destroy();
-
-   //Redirect to login page 
-   header("location: login.php");
+  //Redirect to login page 
+  header("location: login.php");
