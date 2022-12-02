c061e7c837101e7aa8e7eca2a91426b0c84817c3
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-12-02 11:04:45 +0530

    commit

--- Mysql/Mysql.php
+++ Mysql/Mysql.php
@@ -2,15 +2,15 @@
 
 //connecting to the database
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 
 //create a connection
 $conn = mysqli_connect($servername, $username, $password);
 
 //Die if connection was not successful
 if (!$conn) {
-    die("Sorry we failed to connnect: " . mysqli_connect_error());
+    die("Sorry we failed to connnect: " . mysqli_connect_error($conn));
 } else {
     echo "Connection was successful!";
 }
--- Mysql/Mysql_create_db.php
+++ Mysql/Mysql_create_db.php
@@ -1,11 +1,18 @@
 <?php
 //connecting to the database
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 
 //create a connection
 $conn = mysqli_connect($servername, $username, $password);
+
+//Die if connection was not successful
+if (!$conn) {
+    die("Sorry we failed to connnect: " . mysqli_connect_error($conn));
+} else {
+    echo "Connection was successful!<br>";
+}
 
 //Create a db
 $sql = "CREATE DATABASE anil092";
