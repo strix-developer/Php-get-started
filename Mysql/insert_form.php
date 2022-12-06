e7a0bd0d26eff086187bb45a8e708c69dba1a00f
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-12-06 15:22:00 +0530

    commit

--- Mysql/insert_form.php
+++ Mysql/insert_form.php
@@ -11,14 +11,16 @@
         //Connecting to server
         extract($_POST);
         include("create_connection.php");
+        //For checking if Email address already exists or not
         $sql = mysqli_query($conn, "SELECT * FROM users where Email='$email'");
         if (mysqli_num_rows($sql) > 0) {
             echo "Email Id Already Exists";
             exit;
         } else {
 
-            // check if the user has clicked the button "UPLOAD" 
+            // If the user has clicked the button "Submit" 
             if (isset($_POST['submit'])) {
+                //For inserting the Image to the database
                 $fileName = $_FILES["image"]["name"];
                 $fileSize = $_FILES["image"]["size"];
                 $tmpName = $_FILES["image"]["tmp_name"];
@@ -40,9 +42,10 @@
                     VALUES ('$fname', '$lname', '$email', '$pass', '$pnumber','$role','$newImageName')";
 
                     $result = mysqli_query($conn, $sql);
-
+                    //To check if data is submitted successfully or not
                     if ($result) {
                         echo '<strong>Success!</strong>Your entry has been submitted successfully!';
+                        //Redirecting user to the Login page after succcessfully registered
                         header("Location: create_login.php?status=success");
                     } else {
                         echo "The record was not submitted successfully because of this error --->" . mysqli_error($conn);
