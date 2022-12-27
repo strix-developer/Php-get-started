69bbff7b1accf7ea8745eebf1f7539ada154e5b6
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2022-12-27 16:04:03 +0530

    commit

--- wordpress/dashboard.php
+++ wordpress/dashboard.php
@@ -11,7 +11,6 @@
      <title>Mysqli Database</title>
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-
      <style>
          .dropdown:hover .dropdown-menu {
              display: block;
