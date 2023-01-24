3a09bfb870ebab2eba18eac0a3138c9a7bb91b98
anil0092 <anilbhardwaj.strixdeveleopment@gmail.com>
2023-01-24 11:59:55 +0530

    commit

--- jQuery/practice.php
+++ jQuery/practice.php
@@ -5,11 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>jQuery Practice</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script
+     src="https://code.jquery.com/jquery-3.6.3.min.js"
+     integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
+     crossorigin="anonymous">
+    </script>
     <script>
      $(document).ready(function(){
      $("button").click(function(){
-     $("p").hide();
+     $("#para_two").hide();
      });
      });
     </script>
@@ -19,7 +23,7 @@
 <h2>jQuery</h2>
 
 <p>This is a jQuery paragraph.</p>
-<p>This is another jQuery paragraph.</p>
+<p id="para_two">This is another jQuery paragraph.</p>
 
 <button>Click me to hide paragraphs</button>
 
